542de7003821a404ace10d4dc88ba5fc207b0de3
rutecintra <ruteocintra@gmail.com>
2024-07-11 04:50:17 -0300

    feat: Implementing link finder

--- init/schema.sql
+++ init/schema.sql
@@ -23,20 +23,20 @@ CREATE TABLE IF NOT EXISTS 'links' (
     FOREIGN KEY (trip_id) REFERENCES trips(id)
 );
 
--- CREATE TABLE IF NOT EXISTS 'participants' (
---     id TEXT PRIMARY KEY,
---     trip_id TEXT NOT NULL,
---     emails_to_invite_id TEXT NOT NULL,
---     name TEXT NOT NULL,
---     is_confirmed INTEGER, -- 1 for true, 0 for false
---     FOREIGN KEY (trip_id) REFERENCES trips(id),
---     FOREIGN KEY (emails_to_invite_id) REFERENCES emails_to_invite(id)
--- );
+CREATE TABLE IF NOT EXISTS 'participants' (
+    id TEXT PRIMARY KEY,
+    trip_id TEXT NOT NULL,
+    emails_to_invite_id TEXT NOT NULL,
+    name TEXT NOT NULL,
+    is_confirmed INTEGER, -- 1 for true, 0 for false
+    FOREIGN KEY (trip_id) REFERENCES trips(id),
+    FOREIGN KEY (emails_to_invite_id) REFERENCES emails_to_invite(id)
+);
 
--- CREATE TABLE IF NOT EXISTS 'activities' (
---     id TEXT PRIMARY KEY,
---     trip_id TEXT NOT NULL,
---     title TEXT NOT NULL,
---     occurs_at DATETIME,
---     FOREIGN KEY (trip_id) REFERENCES trips(id)
--- );
+CREATE TABLE IF NOT EXISTS 'activities' (
+    id TEXT PRIMARY KEY,
+    trip_id TEXT NOT NULL,
+    title TEXT NOT NULL,
+    occurs_at DATETIME,
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+);
